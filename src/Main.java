ed3099d003ca16d9db5c55df296c89f7d6889dd5
Hoang Nguyen <hoangmegamind1302@gmail.com>
2023-06-13 21:19:30 +0700

    van dang bi loi

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
+    private static final String USER_FILE = "manager.txt";
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int IsRunning = 1;
         while (IsRunning == 1) {
             menu();
-            Scanner sc = new Scanner(System.in);
-            int Option = sc.nextInt();
+            int Option = scanner.nextInt();
             switch(Option) {
                 case 1:
-                    System.out.println("Please sign in");
+                    System.out.println("-------------------------SIGN IN--------------------------");
+                    SignIn(scanner);
                     break;
                 case 2:
                     System.out.println("Please sign up");
@@ -30,5 +35,20 @@ public class Main {
         System.out.print("  Your option is: ");
     }
 
+    public  static void SignIn (Scanner scanner) {
+        System.out.println(" Enter your nick name: ");
+        String userName = scanner.nextLine();
+        System.out.print(" Enter your password: ");
+        String password = scanner.nextLine();
+
+        try {
+            FileWriter fileWriter = new FileWriter(USER_FILE, true);
+            fileWriter.write(userName + " " + password + "\n");
+            fileWriter.close();
+            System.out.println("     Sign in succcessfully.");
+        } catch (IOException e) {
+            System.out.println("     Error: Can not save user information!!!");
+        }
+    }
 
 }
